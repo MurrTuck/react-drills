68177a970dc51fa99f95d6369ba6d0bb3aaf10f8
MurrTuck <murrtuck@gmail.com>
2020-05-13 17:22:55 -0600

    App-3 Complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      superhero: ['iron man', 'capt america', 'black widow', 'hulk', 'thor', 'spider-man', 'ant-man', 'black panther'],
+      filterSuperhero: ''
+    }
+  }
+
+  handleChange(value) {
+    this.setState({ filterSuperhero: value })
+  }
+
+  render() {
+    console.log(this.state)
+    let listOfHero = this.state.superhero.filter((element, index) => {
+      return element.includes(this.state.filterSuperhero)
+    }).map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} />
+        {listOfHero}
+      </div>
+    );
+  }
 }
 
 export default App;
