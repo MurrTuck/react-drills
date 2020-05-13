240f8a43dd53d972a3b2baf9c00011dbe4f3dbc9
MurrTuck <murrtuck@gmail.com>
2020-05-13 15:57:50 -0600

    App-2 Complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      names: ['Murr', 'Kels', 'Bug', 'Benny', 'Noly']
+    }
+  }
+
+
+
+  render() {
+    let newArray = this.state.names.map((element, index) => {
+      return <h1 key={index}>{element}</h1>
+    })
+    return (
+      <div className="App" >
+        {newArray}
+      </div>
+    );
+  }
 }
 
-export default App;
+
